bfd868c69860409fb363a2803e8dcf6a870998ba
DankAlighieri <guilhermeemeterioif@gmail.com>
2025-02-12 15:47:27 -0300

    Controle do quadrado com joystick e borda inicial feita

--- joystickDisplay.c
+++ joystickDisplay.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "pico/stdlib.h"
 #include "hardware/irq.h"
 #include "hardware/adc.h"
@@ -31,13 +32,19 @@
 #define DEAD_ZONE 200
 #define CENTER_VALUE 2048
 
+#define SSD1306_HEIGHT 64
+#define SSD1306_WIDTH 128
+
+#define BORDER_HEIGHT 63
+#define BORDER_WIDTH 127
+
 // Variável para manipular o display
 uint8_t ssd[ssd1306_buffer_length];
 
 // Prepara o canva do display
 struct render_area frame_area = {
     start_column : 0,
-    end_column : ssd1306_width - 1,
+    end_column : BORDER_WIDTH,
     start_page : 0,
     end_page : ssd1306_n_pages - 1
 };
@@ -52,6 +59,10 @@ static volatile bool led_b_state = false;
 
 // Flag para alternar o estado da borda do display
 static volatile bool display_border_alternate = false;
+static bool display_border_on = false;
+
+static uint height_with_square = 55;
+static uint width_with_square = 119;
 
 // Prototipos das funcoes
 static void gpio_irq_handler(uint gpio, uint32_t events);
@@ -60,32 +71,71 @@ static uint setup_pwm(uint pwm_pin);
 static uint16_t read_joystick_axis(uint16_t *vrx, uint16_t *vry);
 static void setup();
 static uint16_t adjust_value(uint16_t value);
+static void draw_square(uint x0, uint y0);
+static void clear_square(uint x0, uint y0);
+static int map(uint raw_coord_x, uint raw_coord_y);
 
 // Varaiveis globais
-uint slice_r;
-uint slice_b;
-uint16_t vrx_value;
-uint16_t vry_value;
+uint slice_r, slice_b;
+uint16_t vrx_value, vry_value;
+uint x,y;
 
 int main() {
     setup();
 
+    int prev_x = 0;
+    int prev_y = 0;
+
     while (true) {
+
         read_joystick_axis(&vrx_value, &vry_value);
+
+        map(vrx_value, vry_value);
 
         vrx_value = adjust_value(vrx_value);
         vry_value = adjust_value(vry_value);
 
-        printf("Valor x: %d\nValor y: %d\n", vrx_value, vry_value);
+        printf("Valor x: %d\nValor y: %d\n", x, y);
         // Atualizando intensidade do LED Vermelho
         pwm_set_gpio_level(LED_R_PIN, vrx_value);
         // Atualizando intensidade do LED Azul
         pwm_set_gpio_level(LED_B_PIN, vry_value);
 
-        // Verifica se o botao do joystick foi pressionado
-        if (display_border_alternate) {
-            /*code*/
-        }
+        // Limpar o quadrado anterior
+        clear_square(prev_x, prev_y);
+        
+        // Desenhar o quadrado no buffer
+        draw_square(x,y);        
+
+        if(display_border_alternate) {
+            if(!display_border_on) {
+                printf("Desenhando borda\n");
+                ssd1306_draw_line(ssd, 0, 0, BORDER_WIDTH, 0, true);
+                ssd1306_draw_line(ssd, 0, 0, 0, BORDER_HEIGHT, true);
+                ssd1306_draw_line(ssd, BORDER_WIDTH, 0, BORDER_WIDTH, BORDER_HEIGHT, true);
+                ssd1306_draw_line(ssd, 0, BORDER_HEIGHT, BORDER_WIDTH, BORDER_HEIGHT, true);
+                height_with_square--;
+                width_with_square--;
+                printf("Borda desenhada\n");
+
+            } else {
+                printf("Limpando borda\n");
+                ssd1306_draw_line(ssd, 0, 0, BORDER_WIDTH, 0, false);
+                ssd1306_draw_line(ssd, 0, 0, 0, BORDER_HEIGHT, false);
+                ssd1306_draw_line(ssd, BORDER_WIDTH, 0, BORDER_WIDTH, BORDER_HEIGHT, false);
+                ssd1306_draw_line(ssd, 0, BORDER_HEIGHT, BORDER_WIDTH, BORDER_HEIGHT, false);
+                height_with_square++;
+                width_with_square++;
+                printf("Borda limpa\n");
+            }
+            display_border_on = !display_border_on;
+            display_border_alternate = !display_border_alternate;
+        }  
+        prev_x = x;
+        prev_y = y;
+    
+        // Renderizar o buffer no display
+        render_on_display(ssd, &frame_area);
         sleep_ms(100);
     }
 }
@@ -108,11 +158,16 @@ static void setup() {
     adc_gpio_init(JOY_Y);
 
     //display
-    i2c_init(i2c1, ssd1306_i2c_clock * 1000);
+    i2c_init(i2c1, 1000000);
     gpio_set_function(DISPLAY_SDA, GPIO_FUNC_I2C);
     gpio_set_function(DISPLAY_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(DISPLAY_SDA);
     gpio_pull_up(DISPLAY_SCL);
+    ssd1306_init();
+
+    calculate_render_area_buffer_length(&frame_area);
+    memset(ssd, 0, ssd1306_buffer_length);
+    render_on_display(ssd, &frame_area);
 
     // Acionando interrupcoes
     gpio_set_irq_enabled_with_callback(BTN_A, GPIO_IRQ_EDGE_FALL, true, gpio_irq_handler);
@@ -125,7 +180,7 @@ static uint setup_pwm(uint pwm_pin) {
 
     pwm_set_clkdiv(slice, CLK_DIV);
     pwm_set_wrap(slice, ADC_WRAP);
-    pwm_set_gpio_level(slice, 0);
+    pwm_set_gpio_level(pwm_pin, 0);
     pwm_set_enabled(slice, true);
 
     //freq = 1,9 KHz
@@ -190,4 +245,23 @@ static uint16_t adjust_value(uint16_t value) {
     } else {
         return 0;
     }
+}
+
+static void draw_square(uint x0, uint y0) {
+    ssd1306_draw_line(ssd, x0, y0, x0 + 8, y0, true);
+    ssd1306_draw_line(ssd, x0, y0, x0, y0 + 8, true);
+    ssd1306_draw_line(ssd, x0, y0 + 8, x0 + 8, y0 + 8, true);
+    ssd1306_draw_line(ssd, x0 + 8, y0, x0 + 8, y0 + 8, true);
+}
+
+static void clear_square(uint x0, uint y0) {
+    ssd1306_draw_line(ssd, x0, y0, x0 + 8, y0, false);
+    ssd1306_draw_line(ssd, x0, y0, x0, y0 + 8, false);
+    ssd1306_draw_line(ssd, x0, y0 + 8, x0 + 8, y0 + 8, false);
+    ssd1306_draw_line(ssd, x0 + 8, y0, x0 + 8, y0 + 8, false);
+}
+
+static int map(uint raw_coord_x, uint raw_coord_y) {
+    x = (raw_coord_x * width_with_square)/4084;
+    y = height_with_square - (raw_coord_y * height_with_square)/4084; // Invertendo o eixo Y
 }
